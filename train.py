558e579102b1701bdbe917b9b65b62d54b2c587d
Kevyn ZHANG <wzhangbu@connect.ust.hk>
2022-01-22 01:40:54 +0800

    debug

--- train.py
+++ train.py
@@ -7,54 +7,41 @@ from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
 from dataset import MyDateSet
 from harnet import HARNet
-import eval
+from eval import eval
 import os
-from utils import weights_init_normal
+from utils import weights_init_normal, save_checkpoint
 
-def main():
+data_set = MyDateSet("./data/6x6_256", "./data/3x3_256")
+train_data = DataLoader(dataset=data_set, num_workers=2, batch_size=1, shuffle=True)
 
-    data_set = MyDateSet("./data/6x6_256", "./data/3x3_256")
-    train_data = DataLoader(dataset=data_set, num_workers=2, batch_size=1, shuffle=True)
+cuda = torch.cuda.is_available()
 
-    cuda = torch.cuda.is_available()
+model = HARNet()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.apply(weights_init_normal)
+mse_loss = nn.MSELoss()
+adam = optim.Adam(model.parameters())
 
-    model = HARNet()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    model.apply(weights_init_normal)
-    mse_loss = nn.MSELoss()
-    adam = optim.Adam(model.parameters())
-    
-    for epoch in range(100):
-        for i, data in enumerate(train_data):
-            x, y = data
-            X, Y = Variable(x), Variable(y)
-            # print(X.data.size())
-            if cuda:
-                X = X.cuda()
-                Y = Y.cuda()
-            out = model(X)
+for epoch in range(100):
+    print("epoch %d/100" %(epoch+1))
+    for i, data in enumerate(train_data):
+        x, y = data
+        X, Y = Variable(x), Variable(y)
+        # print(X.data.size())
+        if cuda:
+            X = X.cuda()
+            Y = Y.cuda()
+        out = model(X)
 
-            adam.zero_grad()
-            loss = mse_loss(out, Y)
-            loss.backward()
-            adam.step()
+        adam.zero_grad()
+        loss = mse_loss(out, Y)
+        loss.backward()
+        adam.step()
 
-            if i % 10 == 0:
-                print("epoch %d/100" %(epoch+1))
-
-        eval(model)
-        save_checkpoint(model, epoch)
-    print('Finished Training')
+    eval(model)
+    save_checkpoint(model, epoch)
+print('Finished Training')
 
 
 
-def save_checkpoint(model, epoch):
-    model_out_path = "checkpoint/" + "model_epoch_{}.pth".format(epoch)
-    state = {"epoch": epoch ,"model": model}
-    if not os.path.exists("./checkpoint/"):
-        os.makedirs("./checkpoint/")
-
-    torch.save(state, model_out_path)
-
-    print("Checkpoint saved to {}".format(model_out_path))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import os
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -8,3 +9,13 @@ def weights_init_normal(m):
     elif classname.find('BatchNorm2d') != -1:
         torch.nn.init.normal(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant(m.bias.data, 0.0)
+
+def save_checkpoint(model, epoch):
+    model_out_path = "checkpoint/" + "model_epoch_{}.pth".format(epoch)
+    state = {"epoch": epoch ,"model": model}
+    if not os.path.exists("./checkpoint/"):
+        os.makedirs("./checkpoint/")
+
+    torch.save(state, model_out_path)
+
+    print("Checkpoint saved to {}".format(model_out_path))
